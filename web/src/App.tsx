6e3c2c8c57ad213d9f47ad87dbfe2d4e6af87a53
Luis Vinicius <viniborges@yahoo.com.br>
2023-01-21 12:07:59 -0300

    Aula 02

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,16 @@
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
-import { Habit } from "./components/Habit";
 
-function App() {
+// import { Habit } from "./components/Habit";
+
+export function App() {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   );
 }
-
-export default App;
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#09090A",
+      },
+      gridTemplateRows: {
+        7: "repeat(7,minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
